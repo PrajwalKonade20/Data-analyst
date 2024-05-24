40c77179cf1b5b5dbe6fa5caafcf87e2cf29e594
PrajwalKonade20 <155521148+PrajwalKonade20@users.noreply.github.com>
2024-05-24 21:57:01 +0530

    Add files via upload

---
+++ inherit.py
@@ -0,0 +1,11 @@
+class demo:
+    def __init__(self,name,id):
+     self.name=name
+     self.id=id
+
+    def showdetails(self):
+     print("name is : {self.name} is {self.id}")
+
+a=demo('Prajwal',101)
+a.showdetails()
+
